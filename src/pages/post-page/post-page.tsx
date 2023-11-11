784a2e356e6eed3e4980203c652dae80d0064d75
AlexandrKataev <dkeyman@mail.ru>
2023-11-11 16:54:43 +0300

    post page

--- src/pages/post-page/post-page.tsx
+++ src/pages/post-page/post-page.tsx
@@ -1,13 +1,22 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './post-page.module.scss';
 import { useGetPostByIdQuery } from '@entities/post';
 
 export const PostPage = () => {
+  const navigate = useNavigate();
   const postId = useParams().postId || 1;
   const { data } = useGetPostByIdQuery(+postId);
   return (
-    <div>
-      <div>{data?.id}</div>
+    <div className={styles.container}>
+      <button className={styles.back} onClick={() => navigate('/')}>
+        {'<= Back'}
+      </button>
+      <h1>Post {data?.id}</h1>
+      <div className={styles.post}>
+        <h2 className={styles.title}>{data?.title}</h2>
+        <div className={styles.body}>{data?.body}</div>
+        <div className={styles.body}>{data?.userId}</div>
+      </div>
     </div>
   );
 };
