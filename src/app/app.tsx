1a977a3ecf870b67188506cac29ad4de6a841b2e
AlexandrKataev <dkeyman@mail.ru>
2023-11-13 22:35:38 +0300

    fix warnings

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
-import { Header } from '@widgets/header';
 import './app.scss';
+
+import { Header } from '@widgets/header';
 import { Routing } from './routing';
 
 export const App = () => {
   return (
     <>
       <Header />
-
       <main>
         <Routing />
       </main>
